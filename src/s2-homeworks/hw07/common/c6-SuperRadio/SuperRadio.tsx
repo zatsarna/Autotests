607d69292dd60e64473cc74c0bbffe98436438b4
zatsarna <lyudazats@gmail.com>
2023-06-11 16:48:25 -0400

    name attribute for input was changed

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -53,7 +53,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       // name, checked, value делают студенты
                       // http://htmlbook.ru/html/input/name
                       checked={o.id ===value }
-                      //name ={o.value}
+                      name ={o.value}
                       value={o.id}
 
                       onChange={onChangeCallback}
